9de66ddd402bdac9c9799185f0fdedff284318cb
defxsec <defxsec@gmail.com>
2023-05-09 19:53:31 -0400

    contenido en español

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aplicación de react - esta es mi Aplicación
         </p>
         <a
           className="App-link"
